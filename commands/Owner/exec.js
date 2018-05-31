04059bcf51ab85e93d409a83864c3c98a5921f7a
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-31 16:53:29 -0400

    edit

--- commands/Owner/exec.js
+++ commands/Owner/exec.js
@@ -1,16 +1,11 @@
 const { Command } = require("frozencord"); // eslint-disable-line
-const { post } = require("snekfetch");
 const execute = require('child_process').exec;
-
-
-
-
 class exec extends Command {
 
     constructor(client) {
         super(client);
-        this.name = "exec";
-        this.description = "execute something";
+        this.name = "invite";
+        this.description = "Invite FireBot.";
         this.guildOnly = true;
         this.ownerOnly = true;
         this.args = [
@@ -20,21 +15,15 @@ class exec extends Command {
                 required: true
             }
         ];
-        this.botPerms = ["SEND_MESSAGES", "EMBED_LINKS"];
-        this.aliases = [];
-
-        run(message, [code], args); {
-            execute(`${args.join(' ')}`, (error, stdout) => {
-                const response = (error || stdout);
-                message.channel.send(`Ran: ${args.join(" ")}\n${response}`, {code: "asciidoc", split: "\n"}).catch(console.error);
-              });
-            }
-        }
-        
     }
 
+    async run (message, [code]) {
+        execute(`${args.join(' ')}`, (error, stdout) => {
+            const response = (error || stdout);
+            message.channel.send(`Ran: ${args.join(" ")}\n${response}`, {code: "asciidoc", split: "\n"}).catch(console.error);
+        })
+    }
 
- 
-
+}
 
 module.exports = exec;
