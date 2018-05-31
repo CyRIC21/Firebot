6f2ef63435a6ea8720813c25e632d7ca8222baa9
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-31 17:02:50 -0400

    nope

--- commands/Owner/exec.js
+++ commands/Owner/exec.js
@@ -17,7 +17,7 @@ class exec extends Command {
         ];
     }
 
-    async run (message, [code], args, join) {
+    async run (message, [code], args) {
         execute(`${args.join(' ')}`, (error, stdout) => {
             const response = (error || stdout);
             message.channel.send(`Ran: ${args.join(" ")}\n${response}`, {code: "asciidoc", split: "\n"}).catch(console.error);
