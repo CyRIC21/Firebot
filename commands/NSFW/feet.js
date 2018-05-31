635f58c2ec82e09572d20548a1a293d774119f67
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-05-31 17:14:15 -0400

    ffs

--- commands/NSFW/feet.js
+++ commands/NSFW/feet.js
@@ -21,7 +21,7 @@ class invite extends Command {
         .get(`https://nekos.life/api/v2/img/feet`);
         if (!message.channel.nsfw) message.reply(" You must be in a N.S.F.W channel to use this command.");
       
-        let hentaiEmbed = new MessageEmbed()
+        let hentaiEmbed = new RichEmbed()
         .setColor("RANDOM")
         .setTitle("Hentai")
         .setImage(body.url)
