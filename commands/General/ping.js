64f3ec0c26d9674905fe142a8ea64da0cf065d68
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-06-01 11:18:19 -0400

    lol

---
+++ commands/General/ping.js
@@ -0,0 +1,29 @@
+const { RichEmbed } = require("discord.js");
+const { Command } = require("frozencord");
+
+
+class ping extends Command {
+
+    constructor(client) {
+        super(client);
+        this.name = "ping";
+        this.description = "Get the latency of FireBot.";
+        this.guildOnly = true;
+        this.args = [
+            {
+                name: "command",
+                type: "string"
+            }
+        ];
+    }
+
+    async run (message) {
+        const embed = new RichEmbed()
+        .setColor(15400990)
+        .setDescription(`${this.client.emojis.find(x => x.name === "loading")} ${this.client.emojis.find(x => x.name === "loading2")} ${this.client.emojis.find(x => x.name === "loading3")} Pong! Time - **${Date.now() - message.createdTimestamp}ms** API Latency is **${Math.round(this.client.ping)}ms** ${this.client.emojis.find(x => x.name === "loading")} ${this.client.emojis.find(x => x.name === "loading2")} ${this.client.emojis.find(x => x.name === "loading3")}`);
+      message.channel.send(embed);
+    }
+
+}
+
+module.exports = ping;
