45cd6316a0b5bb5b5de7d67b5ace7d8dfefeebfd
ur-mums-a-trap21 <mcalts71@gmail.com>
2018-06-02 12:48:00 -0400

    add events

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const fs = require("fs");
 app.get("/", (request, response) => {
 //  console.log(Date.now() + " Ping Received");
   response.sendStatus(200);
@@ -22,5 +23,19 @@ const client = new FrozenClient({
         type: "PLAYING"
     } 
 });
+
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    this.client.on(eventName, event.bind(null, this.client));
+    delete require.cache[require.resolve(`./events/${file}`)];
+  });
+});
+
+
 
 client.login(process.env.TOKEN);
